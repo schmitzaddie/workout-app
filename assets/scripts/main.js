0f644444ae090a84c561b126b09fe66c4d4e743e
Dylan Theno <djtheno@gmail.com>
2019-01-19 10:27:35 -0600

    firebase

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,37 @@
 
-$( document ).ready(function() {
+$(document).ready(function () {
+    var config = {
+        apiKey: "AIzaSyBQvk3ZKGc9LMbus4Ma39xV_NPobuQOu0o",
+        authDomain: "workout-app-4ece3.firebaseapp.com",
+        databaseURL: "https://workout-app-4ece3.firebaseio.com",
+        projectId: "workout-app-4ece3",
+        storageBucket: "workout-app-4ece3.appspot.com",
+        messagingSenderId: "1003379802026"
+       
+      };
+      firebase.initializeApp(config);
+
+
+
+      var database = firebase.database();
+
+     function playRandomSound() {
+
+
+
+        //An array to house all of the URLs of your sounds
+        //insert sas file//
+        var sounds = [];
+
+        //This line will select a random sound to play out of your provided 
+        var soundfile = sounds[Math.floor(Math.random() * sounds.length)];
+
+        //Find the player element that you created and generate an embed file to play the sound within it
+        document.getElementById("inspo").innerHTML = "<embed src=\"" + soundfile + "\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+    }
+
+
    
 
-
-function playRandomSound(){
-
-    //An array to house all of the URLs of your sounds
-    //insert sas file//
-    var sounds = [];
-    
-    //This line will select a random sound to play out of your provided URLS
-    var soundfile = sounds[Math.floor(Math.random()*sounds.length)];
-    
-    //Find the player element that you created and generate an embed file to play the sound within it
-    document.getElementById("player").innerHTML="<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
- }
 
 });
